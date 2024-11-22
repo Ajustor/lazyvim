ca87c09989a156d19ce79db9626b19a2d2a71b56
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-11-22 19:11:25 +0100

    adding golang support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "biome", "ts_ls" }
+local servers = { "html", "cssls", "biome", "ts_ls", "gopls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -13,8 +13,30 @@ for _, lsp in ipairs(servers) do
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
+    opts = {
+      inlay_hint = { enabled = true },
+    },
   }
 end
+
+local util = require "lspconfig/util"
+
+lspconfig.gopls.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>qq", "<cmd>:quitall<CR>", {desc = "Quit all neovim instance"})
+map("n", "<C-s>", "<cmd>:w<CR>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
