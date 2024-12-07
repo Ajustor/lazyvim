2d50385e33a10b223a5d78e6fc72ba9bf4b06713
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-12-07 19:57:28 +0100

    Feat(new-version): debugger

--- lua/plugins/js-ts-stuff.lua
+++ lua/plugins/js-ts-stuff.lua
@@ -1,11 +1,12 @@
 return {
-    {
-        "typed-rocks/ts-worksheet-neovim",
-        opts = {
-            severity = vim.diagnostic.severity.WARN,
-        },
-        config = function(_, opts)
-            require("tsw").setup(opts)
-        end
+  {
+    "typed-rocks/ts-worksheet-neovim",
+    opts = {
+      severity = vim.diagnostic.severity.WARN,
     },
+    config = function(_, opts)
+      require("tsw").setup(opts)
+    end,
+  },
+  { "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } },
 }
